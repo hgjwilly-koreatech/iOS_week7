7a3bdc211428d3824f6944fad18e6d38c9df31bc
Hong Gi Jeong <hgjwilly@gmail.com>
2025-05-21 15:44:52 +0900

    Initial Commit

---
+++ week7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week7
+//
+//  Created by 홍기정 on 5/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
